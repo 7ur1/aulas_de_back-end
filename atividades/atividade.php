74ac4e22ca9d4145aea9569781def3c1d9e1f1b5
Yuri GonÃ§alves Fernandes <14174175608@dc01.ifs.ifsuldeminas.edu.br>
2023-02-23 10:06:34 -0300

    Carga Inicial

---
+++ atividades/atividade.php
@@ -0,0 +1,28 @@
+<?php
+$nota = 6;
+$faltas = 74;
+if($nota >= 6 && $faltas < 75){ 
+    echo " foi aprovado!";
+}else{
+    echo " foi reprovado!";
+}
+?>
+
+<strong>Exemplo 2</strong>
+
+<p>O aulo para ser aprovado nprecisa obter 
+    nota superior a 6 pontos ,para fazer o exame final 
+    precisa ter tirado menos que 3 pontos.
+    Notas menores que 3 são reprovações diretas 
+</p>
+
+<?php
+$nota = 2;
+if($nota >= 6){ 
+    echo "Aprovado!";
+}else if($nota > 3){
+    echo "Faz exame final!";
+}else{
+    echo "Reprovado!";
+}
+?>

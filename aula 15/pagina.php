f9e9b03a97670dee94c8fceb8b6837794d3f9eeb
Yuri GonÃ§alves Fernandes <14174175608@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-27 08:45:26 -0300

    aula15

---
+++ aula 15/pagina.php
@@ -0,0 +1,32 @@
+<?php
+$cor = ["Yellow","Green","Blue","Pink","Red","Purple","Orange","Grey","White"];
+foreach($cor as $key => $cor){
+    echo "<a href='pagina.php?cor=$cor'> Mudar para $cor </a>" ."<br>";
+}
+
+if(isset($_GET["cor"])){
+    $cor = $_GET["cor"];
+}else{
+    $cor  ="white";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$cor = ["Yellow","Green","Blue","Pink","Red","Purple","Orange","Grey","White"];
+foreach($cor as $key => $cor){
+    echo "<a href='pagina.php?cor=$cor'> Mudar para $cor </a>" ."<br>";
+}
+?>
+
+</body>
+</html>

3dee40f8759c6adf85ebcd97f7b1da8a4d979c54
Yuri GonÃ§alves Fernandes <14174175608@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-05-18 10:31:17 -0300

    aula17

--- aula 17/produto/inserir.php
+++ aula 17/produto/inserir.php
@@ -2,12 +2,17 @@
 
 require_once "../conexao.php";
   
-if(isset($_POST["nome"]) && isset($_POST["descricao"])&& isset($_POST["preco"]) && isset($_POST["foto"]))
+if(isset($_POST["nome"]) && isset($_POST["descricao"])&& isset($_POST["preco"]))
+
 {
+
+//inclui o arquivo para salvar_foto.php do uploads 
+require_once "salvar_foto.php";
+
 $nome = $_POST["nome"];
 $descricao = $_POST["descricao"];
 $preco = $_POST["preco"];
-$foto = "semfoto.png";
+$foto = $nome_arquivo;
 
 //String com o comando SQL para ser executado no DB
 $sql = "INSERT INTO `produto` (`nome`, `descricao`, `preco`, `foto`) VALUES (?, ?, ?, ?);";
